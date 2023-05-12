84824adbcc5bc268bc4f1147507a48e78b841ff3
patr4519 <jero96@mail.ru>
2023-05-12 16:28:06 +0300

    Added start and stop buttons

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,37 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Timer: React.FC = () => {
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [hours, setHours] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds((prevSeconds) => prevSeconds + 1);
-
-      if (seconds >= 59) {
-        setSeconds(0);
-        setMinutes((prevMinutes) => prevMinutes + 1);
-      }
-
-      if (minutes >= 59) {
-        setMinutes(0);
-        setHours((prevHours) => prevHours + 1);
-      }
-    }, 1000);
+    let interval: ReturnType<typeof setInterval>;
+    
+    if (isRunning) {
+        interval = setInterval(() => {
+            setSeconds((prevSeconds) => prevSeconds + 1);
+    
+            if (seconds >= 59) {
+              setSeconds(0);
+              setMinutes((prevMinutes) => prevMinutes + 1);
+            }
+    
+            if (minutes >= 59) {
+              setMinutes(0);
+              setHours((prevHours) => prevHours + 1);
+            }
+          }, 1000);
+    }
 
     return () => clearInterval(interval);
-  }, [seconds, minutes, hours]);
+  }, [seconds, minutes, hours, isRunning]);
 
   const formatTime = (value: number): string => {
-    return value.toString().padStart(2, '0');
+    return value.toString().padStart(2, "0");
   };
-
-  console.log(formatTime(1))
 
   return (
     <div>
-      <h1>{`${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`}</h1>
+      <h1>{`${formatTime(hours)}:${formatTime(minutes)}:${formatTime(
+        seconds
+      )}`}</h1>
+      <button onClick={() => setIsRunning(true)}>Start</button>
+      <button onClick={() => setIsRunning(false)}>Pause</button>
     </div>
   );
 };
